ad40ef8f0047bf102cf2294c6161ea8d43a50806
sudo180272 <sudo180272@protonmail.ch>
2021-07-10 03:35:16 +0900

    prepare environment

---
+++ src/controllers/app.ts
@@ -0,0 +1,13 @@
+import koa from 'koa';
+
+const app = new koa()
+const port = 8100
+
+app.use(ctx => {
+  ctx.body = "Hello World!"
+})
+
+app.listen(port, () => {
+  console.log("Server running on " + port)
+})
+

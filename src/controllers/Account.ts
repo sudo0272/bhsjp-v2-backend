5fdcce336c0a50b8da501b6c9ad3caaaa593ce86
sudo180272 <sudo180272@protonmail.ch>
2021-07-10 22:41:40 +0900

    Account: implement update functions

--- src/controllers/Account.ts
+++ src/controllers/Account.ts
@@ -6,6 +6,7 @@ import {
 import {
   promisify
 } from 'util'
+
 class Account {
   private mysqlManager: MysqlManager
   private generateSalt: () => Promise<string>
@@ -17,19 +18,94 @@ class Account {
     this.hash = promisify<string, string, string>(hashPassword)
   }
 
-  public async createUser(username: string, nickname: string, password: string, email: string): Promise<void> {
-    return new Promise(async (resolve) => {
-      const salt = await this.generateSalt()
-      const hashedPassword = await this.hash(password, salt)
+  private async generateSaltAndPassword(password: string): Promise<string[]> {
+    const salt = await this.generateSalt()
+    const hashedPassword = await this.hash(password, salt)
 
+    return [salt, hashedPassword]
+  }
+
+  public async createUser(username: string, nickname: string, password: string, email: string): Promise<void> {
+    const [salt, hashedPassword] = await this.generateSaltAndPassword(password)
+
+    try {
       await this.mysqlManager.query(
-        "insert into `Account` (`username`, `nickname`, `password`, `salt`, `email`, `level`) values (?, ?, ?, ?, ?, ?)",
+        "INSERT" +
+        "  INTO `Account`" +
+        "  (`username`, `nickname`, `password`, `salt`, `email`, `level`)" +
+        "  VALUES (?, ?, ?, ?, ?, ?)",
         [username, nickname, hashedPassword, salt, email, 1]
       )
-      setTimeout(async () => {
-        resolve()
-      }, 2000)
-    })
+    } catch (e) {
+      throw e
+    }
+  }
+
+  public async updateNickname(id: number, nickname: string): Promise<void> {
+    try {
+      await this.mysqlManager.query(
+        "UPDATE `Account`" +
+        "  SET `nickname` = ?" +
+        "  WHERE `id` = ?",
+        [nickname, id]
+      )
+    } catch (e) {
+      throw e
+    }
+  }
+
+  public async updatePassword(id: number, password: string) {
+    const [salt, hashedPassword] = await this.generateSaltAndPassword(password)
+
+    try {
+      await this.mysqlManager.query(
+        "UPDATE `Account`" +
+        "  SET `password` = ?, `salt` = ?" +
+        "  WHERE `id` = ?",
+        [hashedPassword, salt, id]
+      )
+    } catch (e) {
+      throw e
+    }
+  }
+
+  public async updateEmail(id: number, email: string) {
+    try {
+      await this.mysqlManager.query(
+        "UPDATE `Account`" +
+        "  SET `email` = ?" +
+        "  WHERE `id` = ?",
+        [email, id]
+      )
+    } catch (e) {
+      throw e
+    }
+  }
+
+  public async increaseLevel(id: number) {
+    try {
+      await this.mysqlManager.query(
+        "UPDATE `Account`" +
+        "  SET `level` = `level` + 1" +
+        "  WHERE `id` = ?",
+        [id]
+      )
+    } catch (e) {
+      throw e
+    }
+  }
+
+  public async decreaseLevel(id: number) {
+    try {
+      await this.mysqlManager.query(
+        "UPDATE `Account`" +
+        "  SET `level` = `level` - 1" +
+        "  WHERE `id` = ?",
+        [id]
+      )
+    } catch (e) {
+      throw e
+    }
   }
 }
 
